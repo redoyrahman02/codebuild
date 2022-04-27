b06dd9da5ae04d5e3915b8a53f01890d211bd3bd
Md atiqur Rahman <redoyrahman02@gmail.com>
2022-04-27 13:19:08 +0600

    Update supervisor_install.sh
    supervisor related commands

--- supervisor_install.sh
+++ supervisor_install.sh
@@ -2,3 +2,9 @@ sudo amazon-linux-extras install epel
 
 # then install supervisor
 sudo yum install supervisor
+
+# start supervisor 
+sudo systemctl start supervisord
+
+# enable for auto start on reboot 
+sudo systemctl enable supervisord
